e3d49f4841257ca133ad21ff04db1fa6154d7640
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-17 15:41:11 -0500

    Base happy path implemented

--- fetcher.js
+++ fetcher.js
@@ -9,15 +9,20 @@ const LOCAL_PATH = arguments[1];
 
 // TODO: perform check on args
 
-// TODO: HTTP request
-
 request(URL, (error, response, body) => {
   
   if (error){
     return console.error('Request failed: ', error);
   }
 
-  // TODO: writing recieved data to file
+  fs.writeFile(LOCAL_PATH, body, err => {
+    if (err) {
+      console.error("Write to disk failed: ", err);
+    }
+
+    // Success message(s)
+    console.log(`Downloaded and saved ${body.length} bytes to ${LOCAL_PATH}`)
+  });
 
 });
 
