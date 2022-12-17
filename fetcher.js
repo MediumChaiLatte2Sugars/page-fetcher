a5ef9f2176c7d5cfd6552d733659b9f02d48f106
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-17 15:32:57 -0500

    Fixed imports, barebones set up of code

--- fetcher.js
+++ fetcher.js
@@ -1,2 +1,23 @@
-const arguments = process.argv.slice(2);
+
 const request = require('request');
+const fs = require('fs');
+
+// Setting required arugments
+const arguments = process.argv.slice(2);
+const URL = arguments[0];
+const LOCAL_PATH = arguments[1];
+
+// TODO: perform check on args
+
+// TODO: HTTP request
+
+request(URL, (error, response, body) => {
+  
+  if (error){
+    return console.error('Request failed: ', error);
+  }
+
+  // TODO: writing recieved data to file
+
+});
+
