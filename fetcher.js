4cbaa656651008001e81c4eb65f11bf65e06e9b5
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-17 21:06:46 -0500

    Error fixes

--- fetcher.js
+++ fetcher.js
@@ -28,26 +28,27 @@ request(URL, (error, response, body) => {
 
   // Check to see if we have successful access to provided URL
   if (!(response.statusCode <= 299 && response.statusCode >= 200)) {
-    return console.error("Invalid URL provided: ", new Error(`Status: ${response.statusCode}`));
+    throw new Error(`Invalid URL! Status: ${response.statusCode}`);
   }
 
   // Check on provided path
-  fs.access(path.format(newPath), fs.F_Ok , (err) => {
-    
+  fs.access(path.format(newPath), fs.F_Ok, (err) => {
+
     if (err) {
-      return console.error("Ivalid path provided: ", err);
+      console.log("Invalid Path!", err);
+      process.exit();
     }
-    
+
     // Check if file exists
-    fs.access(INSTALL_PATH, fs.F_Ok , (err) => {  
+    fs.access(INSTALL_PATH, fs.F_Ok, (err) => {
 
       // File doesn't exist
-      if (err){
-       writeToFile();
-       console.log(`Downloaded and saved ${body.length} bytes to ${INSTALL_PATH}`);
-       process.exit();
+      if (err) {
+        writeToFile();
+        console.log(`Downloaded and saved ${body.length} bytes to ${INSTALL_PATH}`);
+        process.exit();
       }
-       
+
       // Prompt user for control flow decision
       rl.question("File exists, overwrite (Y/N)? ", (answer) => {
 
@@ -61,7 +62,7 @@ request(URL, (error, response, body) => {
         }
 
         process.exit();
-    });
+      });
 
     });
   });
@@ -70,9 +71,10 @@ request(URL, (error, response, body) => {
 
     fs.writeFile(INSTALL_PATH, body, err => {
       if (err) {
-        console.error("Write to disk failed: ", err);
+        console.log("Write to disk failed!", err);
+        process.exit();
       }
     });
-  
-  }
+
+  };
 });
